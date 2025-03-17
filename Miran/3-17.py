04a018c74035cd93955636877d2af3c48afa4af7
meran26 <meran26@naver.com>
2025-03-17 21:49:25 +0900

    3-17

---
+++ Miran/3-17.py
@@ -0,0 +1,17 @@
+N = int(input())
+word = [input() for _ in range(N)]
+cnt = 0
+
+for w in word:
+    check_word = []
+    check = True
+    for i in range(len(w)):
+        if w[i] not in check_word:
+            check_word.append(w[i])
+        elif w[i] in check_word and w[i-1] != w[i]:
+            check = False
+            break
+    if check is True:
+        cnt += 1
+
+print(cnt)
